8c7bddb4d29f14a3744ba4ccb295d924d3f80d39
holms <anna61c9@edu.zealand.dk>
2022-11-08 12:38:46 +0100

    Initial commit

---
+++ insert.php
@@ -0,0 +1,84 @@
+<?php
+require "settings/init.php";
+
+if (!empty($_POST["data"])){
+    $data = $_POST["data"];
+
+    $sql = "INSERT INTO produkter (prodNavn, prodBeskrivelse, prodPris) VALUES(:prodNavn, :prodBeskrivelse, :prodPris)";
+$bind = [":prodNavn" => $data["prodNavn"], ":prodBeskrivelse" => $data["prodBeskrivelse"], ":prodPris" => $data["prodPris"]];
+
+    $db->sql($sql, $bind,false);
+
+    echo "Produktet er nu sat ind . <a href='insert.php'>Indsæt flere produkter</a>";
+    exit;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>indsæt til database</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="https://cdn.tiny.cloud/1/ys9ivv1x233eg2mrq0d4prcpenkssm67kjeagomz2eix9ubu/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+
+<body>
+
+<form method="post" action="insert.php">
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <div class="form-group">
+                <label for="prodNavn">Produkt navn</label>
+                <input class="form-control" type="text" name="data[prodNavn]" id="" placeholder="" value="">
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="form-group">
+                <label for="prodPris">Produkt pris</label>
+                <input class="form-control" type="number" step="0.1" name="data[prodPris]" id="" placeholder="" value="">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="form-group">
+                <label for="prodBeskrivelse">Produkt beskrivelse</label>
+                <textarea class="form-control" type="number" step="0.1" name="data[prodBeskrivelse]" id="" placeholder="" value=""></textarea>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 offset-md-6">
+<button class="form-control btn btn-primary" type="submit" id="btnSubmit">Opret produkt</button>
+        </div>
+    </div>
+</form>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<script>
+    tinymce.init({
+        selector: 'textarea',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+            { value: 'First.Name', title: 'First Name' },
+            { value: 'Email', title: 'Email' },
+        ]
+    });
+</script>
+
+
+</body>
+</html>
