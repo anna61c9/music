bcaeb1a5824e177d2eb5ce8f3b77a93e585ba11f
and20 <and2000mib@gmail.com>
2022-11-10 09:39:53 +0100

    database og billeder

--- index.php
+++ index.php
@@ -1,8 +1,41 @@
 <?php
 require "settings/init.php";
 
-$blogs = $db->sql("SELECT * FROM blogs");
+$musik = $db->sql("SELECT * FROM musik");
 
-foreach ($blogs as $blog){
-    echo $blog->blogName . "<br>";
+?>
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Music</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="https://cdn.tiny.cloud/1/ys9ivv1x233eg2mrq0d4prcpenkssm67kjeagomz2eix9ubu/tinymce/6/tinymce.min.js"
+            referrerpolicy="origin"></script>
+</head>
+
+<body>
+
+<?php
+
+foreach ($musik as $musikken){
+    echo $musikken->musikTitel . " - " . $musikken->musikKunsnter . "<br>";
 }
+
+?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
