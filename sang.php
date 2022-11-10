db5f5143d9fd2e8b0411c90bf16c44e2a67590f5
holms <anna61c9@edu.zealand.dk>
2022-11-10 10:57:17 +0100

    Lavet ekstra php side

--- sang.php
+++ sang.php
@@ -54,39 +54,36 @@
 <br><br>
 
 
-<div class="container-fluid justify-content-center">
-    <div class="row">
-
-        <div class="col-8">
-            <h1 style="color: #62388e">Liste</h1>
-        </div>
-        <div class="col-2 m-3 justify-content-end">
-            <select class="form-select bg-minfarve border-minfarve" style="color:#ced4da"
-                    aria-label="Default select example">
-                <option selected style="color:#ced4da">Top 10</option>
-                <option value="1" style="color:#ced4da">Genre</option>
-                <option value="2" style="color:#ced4da">Alphabetic</option>
-            </select>
-        </div>
-    </div>
-
+<div class="container beskrivelse justify-content-center">
 
     <div class="row">
-        <div class="card col-12 col-md-6 col-lg-5 p-2 m-3 shadow">
-            <div class="row">
-                <div class="col-4">
-                    <img src="images/ace%20of%20spades.jpg" class="card-img-top" alt="cover"></div>
-                <div class="col-8">
-                    <div class="card-body">
-                        <a href="sang.php"><h5 class="card-title">Card title</h5></a>
-                        <p class="card-text" style="color: #7f8793">Some quick example text to build on the</p>
-                        <a href="#"><img src="images/download.png" alt="social"></a>
-                    </div>
-                </div>
+        <div class="card col-12 col-md-6 col-lg-5 p-2 m-3">
+            <img src="images/ace%20of%20spades.jpg" class="card-img-top" style="width: 500vh" alt="cover">
+        </div>
+
+        <div class="card col-12 col-md-6 col-lg-5 p-2 m-3">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text" style="color: #7f8793">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce quis lectus quis sem lacinia nonummy. Proin mollis lorem non dolor.
+                    In hac habitasse platea dictumst. Nulla ultrices odio.
+                    Donec augue. Phasellus dui. Maecenas facilisis nisl vitae nibh.
+                    Proin vel seo est vitae eros pretium dignissim.
+                    Aliquam aliquam sodales orci. Suspendisse potenti.
+                   </p> <br>
+
+                <p class="card-text" style="color: #7f8793"> Nunc adipiscing euismod arcu. Quisque facilisis mattis lacus.
+                    Fusce bibendum, velit in venenatis viverra, tellus ligula dignissim felis,
+                    quis euismod mauris tellus ut urna. Proin scelerisque.
+                    Nulla in mi. Integer ac leo. Nunc urna ligula, gravida a,
+                    pretium vitae, bibendum nec, ante. Aliquam ullamcorper
+                    iaculis lectus. Sed vel dui. Etiam lacinia risus vitae lacus.
+                    Aliquam elementum imperdiet turpis.</p>
+
+                <br>
+                <a href="#"><img src="images/download.png" alt="social"></a>
             </div>
         </div>
-
-
     </div>
 </div>
 
