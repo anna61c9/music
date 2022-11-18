62528dfa6621eb32d39a9e9ee9873fdd2718fee9
and20 <and2000mib@gmail.com>
2022-11-18 14:15:54 +0100

    index samlet skal lige ha kortne op på en række af 2 istedet for en række af 1

--- index.php
+++ index.php
@@ -78,39 +78,39 @@ $musik = $db->sql("SELECT * FROM musik");
 </div>
 
 <?php
-foreach ($musik as $musikken){
+foreach ($musik as $musikken) {
     ?>
 
-    <div class="container-fluid">
+    <div class="container">
         <div class="card col-12 col-md-6 col-lg-5 p-2 m-3 shadow">
             <div class="row">
                 <div class="col-4">
-            <?php
-            echo "<img src='images/".$musikken->musikBilled. "' class='card-img-top' alt='cover'>"
-            ?>
-        </div>
-        <div class="col-8">
-            <div class="card-body">
-                <a href="sang.php"><h5 class="card-title"><?php echo $musikken->musikTitel; ?></h5></a>
-                <p class="card-text" style="color: #7f8793">     <?php
-                    echo $musikken->musikKunstner
-                    ?>
-                    <br>
                     <?php
-                    echo $musikken->musikAlbum
-                    ?></p>
-                <?php
-                echo $musikken->musikTid
-                ?>
-                <?php
-                echo $musikken->musikDato
-                ?>
+                    echo "<img src='images/" . $musikken->musikBilled . "' class='card-img-top' alt='cover'>"
+                    ?>
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <a href="sang.php"><h5 class="card-title"><?php echo $musikken->musikTitel; ?></h5></a>
+                        <p class="card-text" style="color: #7f8793">     <?php
+                            echo $musikken->musikKunstner
+                            ?>
+                            <br>
+                            <?php
+                            echo $musikken->musikAlbum
+                            ?></p>
+                        <?php
+                        echo $musikken->musikTid
+                        ?>
+                        <?php
+                        echo $musikken->musikDato
+                        ?>
+                    </div>
+                </div>
             </div>
+        </div>
     </div>
-</div>
-</div>
-    </div>
-  <?php
+    <?php
 }
 ?>
 
